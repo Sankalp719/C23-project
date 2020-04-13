ba19dc20bb0bfdbb4daa4f75d7a455b49158d1ad
Sankalp719 <62709399+Sankalp719@users.noreply.github.com>
2020-04-13 19:24:51 +0530

    Add files via upload
    C23 Project

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+
+function setup(){
+    var canvas = createCanvas(400,400);
+     createSprite (200,285,192,210);
+     createSprite (50,360,80,80);
+     createSprite (350,360,85,85);
+     createSprite (35,290,50,50);
+     createSprite (365,289,50,50);
+     createSprite(290,155,40,40);
+     createSprite(110,155,40,40);
+     createSprite(205,155,40,40);
+
+     
+     
+     
+     createSprite (200, 395,400,10);
+}
+
+      function draw(){
+       background(255);
+       drawSprites();
+    }   
